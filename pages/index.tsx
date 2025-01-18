7a8a725bc13dc7d515b7600766d9f317d891f2b8
Qiqian <qichaiqian02@gmail.com>
2025-01-18 15:29:37 +0800

    update skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -553,15 +553,27 @@ export default function Home() {
               </div>
               <div className='bg-gray-100 p-6 rounded-lg hover:scale-[102%] transition-transform'>
                 <h4 className='text-base md:text-lg font-semibold mb-5 text-center md:text-left'>Languages</h4>
-                <div className="transition-transform transform-gpu hover:scale-[102%]  group text-base md:text-lg">
+                <div className="transition-transform transform-gpu hover:scale-[102%] group text-base md:text-lg">
                   <p className=" py-2 ">
-                    <Image src="/python.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
-                    Python <span className="text-sm hidden transition-transform transform-gpu group-hover:inline-block"> (70%) </span>
+                    <Image src="/ts.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
+                    Typescript <span className="text-sm hidden transition-transform transform-gpu group-hover:inline-block"> (60%) </span>
                   </p>
                   <div className="w-full bg-gray-200 rounded-full h-2 md:h-3 dark:bg-gray-700 mb-2 relative group">
                     <div
                       className="bg-blue-500 h-2 md:h-3 rounded-full"
-                      style={{ width: "70%" }}
+                      style={{ width: "60%" }}
+                    ></div>
+                  </div>
+                </div>
+                <div className="transition-transform transform-gpu hover:scale-[102%] group text-base md:text-lg">
+                  <p className=" py-2 ">
+                    <Image src="/java.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
+                    Java <span className="text-sm hidden transition-transform transform-gpu group-hover:inline-block"> (60%) </span>
+                  </p>
+                  <div className="w-full bg-gray-200 rounded-full h-2 md:h-3 dark:bg-gray-700 mb-2 relative group">
+                    <div
+                      className="bg-blue-500 h-2 md:h-3 rounded-full"
+                      style={{ width: "60%" }}
                     ></div>
                   </div>
                 </div>
@@ -577,33 +589,9 @@ export default function Home() {
                     ></div>
                   </div>
                 </div>
-                <div className="transition-transform transform-gpu hover:scale-[102%] group text-base md:text-lg">
-                  <p className=" py-2 ">
-                    <Image src="/rprogramming.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
-                    R Programming <span className="text-sm hidden transition-transform transform-gpu group-hover:inline-block"> (70%) </span>
-                  </p>
-                  <div className="w-full bg-gray-200 rounded-full h-2 md:h-3 dark:bg-gray-700 relative group">
-                    <div
-                      className="bg-blue-500 h-2 md:h-3 rounded-full"
-                      style={{ width: "70%" }}
-                    ></div>
-                  </div>
-                </div>
               </div>
               <div className='bg-gray-100 p-6 rounded-lg hover:scale-[102%] transition-transform'>
                 <h4 className='text-base md:text-lg font-semibold mb-5 text-center md:text-left'>Tools</h4>
-                <div className="transition-transform transform-gpu hover:scale-[102%] group text-base md:text-lg">
-                  <p className=" py-2 ">
-                    <Image src="/ts.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
-                    Typescript <span className="text-sm hidden transition-transform transform-gpu group-hover:inline-block"> (60%) </span>
-                  </p>
-                  <div className="w-full bg-gray-200 rounded-full h-2 md:h-3 dark:bg-gray-700 mb-2 relative group">
-                    <div
-                      className="bg-blue-500 h-2 md:h-3 rounded-full"
-                      style={{ width: "60%" }}
-                    ></div>
-                  </div>
-                </div>
                 <div className="transition-transform transform-gpu hover:scale-[102%] group text-base md:text-lg">
                   <p className=" py-2 ">
                     <Image src="/git.png" alt="" className="inline-block w-8 h-8 mr-2" width={240} height={240} />
